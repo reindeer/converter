a5d2721c436ece222842e56c82d634f805865cae
Denis Daschenko <reindeer@tarandro.ru>
2021-05-24 21:23:00 +0300

    Add amount option support

--- src/Command/CurrencyRate.php
+++ src/Command/CurrencyRate.php
@@ -21,6 +21,8 @@ class CurrencyRate extends Command
 
     public const OPTION_TO_CURRENCY = 'to';
 
+    public const OPTION_AMOUNT = 'amount';
+
     protected static $defaultName = 'fetch-currency';
 
     public function __construct(
@@ -36,6 +38,7 @@ class CurrencyRate extends Command
             ->setDescription('Fetch currency rates from CBR')
             ->addOption(self::OPTION_DATE, 'd', InputArgument::OPTIONAL, 'ISO Date', date('Y-m-d'))
             ->addOption(self::OPTION_FROM_CURRENCY, 'f', InputArgument::OPTIONAL, 'From currency', CurrencyInterface::BASE_CURRENCY)
+            ->addOption(self::OPTION_AMOUNT, 'a', InputArgument::OPTIONAL, 'Amount to convert')
             ->addArgument(self::OPTION_TO_CURRENCY, InputArgument::OPTIONAL, 'To currency', static::DEFAULT_CURRENCY);
     }
 
@@ -49,13 +52,33 @@ class CurrencyRate extends Command
             $output->writeln("<error>Failed to parse date: {$input->getOption(self::OPTION_DATE)}</error>");
             return Command::FAILURE;
         }
+        $amount = $input->getOption(self::OPTION_AMOUNT);
 
         try {
             $rate = $this->rateRepository->get($fromCurrency, $toCurrency, $date);
 
             $em = $rate->getDelta() >= 0 ? '<info>+%.4f</info>' : '<error>%.4f</error>';
             $output->writeln("Currency rate on <comment>{$rate->getDate()->format('d.m.Y')}</comment>");
-            $output->writeln(sprintf("%s -> %s: %.4f $em", strtoupper($rate->getBaseCurrency()), strtoupper($rate->getTargetCurrency()), $rate->getRate(), $rate->getDelta()));
+            if (!$amount) {
+                $output->writeln(sprintf(
+                    "1%s = %.4f%s $em",
+                    strtoupper($rate->getTargetCurrency()),
+                    $rate->getRate(),
+                    strtoupper($rate->getBaseCurrency()),
+                    $rate->getDelta(),
+                ));
+            } else {
+                $output->writeln(sprintf(
+                    "%.2f%s = %.2f%s (%.4f) $em",
+                    $amount,
+                    strtoupper($rate->getTargetCurrency()),
+                    $rate->getRate() * $amount,
+                    strtoupper($rate->getBaseCurrency()),
+                    $rate->getRate(),
+                    $rate->getDelta(),
+                ));
+            }
+
             return Command::SUCCESS;
         } catch (CurrencyNotFoundException $e) {
             $output->writeln("<error>Currency {$e->getCurrencyCode()} is not found</error>");
